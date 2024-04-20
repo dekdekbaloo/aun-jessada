2df8eb84852de5f69db1154112c09b863e3b7bfd
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2024-04-21 00:13:41 +0900

    [init] First commit with header

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,40 @@
 module.exports = {
   siteMetadata: {
     title: `aun-jessada`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Inter`,
+            file: `https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Prompt`,
+            file: `https://fonts.googleapis.com/css2?family=Prompt:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
